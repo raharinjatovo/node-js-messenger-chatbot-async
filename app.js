ee1430756ef309a2358919c14ce429a5c94e7b0a
raharinjatovo <raharinjatovo.a@zurcher.edu.mg>
2023-01-09 16:29:55 +0300

    insert notice fird

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
- * pass word adventis server:  1Zd8dq1G+z.E2P
+ * 
  */
 
 /* jshint node: true, devel: true */
